05b4384cf19838d4e15075d13f5fcdf233040a82
OLGA\_KUZKINA\_OLGA <o.kuzkina@gmail.com>
2023-01-09 10:38:25 +0100

    added variable

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const myInfo = require("./information");
 const cowsay = require("cowsay");
 
 console.log(
-  `Hi, my name is ${myInfo.name} and I'm in campus ${myInfo.campus} for study`
-);
-
-console.log(
   cowsay.think({
-    text: "I'm a moooodule",
+    text: `Hi, my name is ${myInfo.name} and I'm in campus ${myInfo.campus} for study`,
     e: "oO",
     T: "U ",
   })
